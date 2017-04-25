ca9ea04d46e53b32d0b5872c54140f60aba0d8a2
Enzo Cano <enzo.cano@southworks.com>
2017-04-25 11:19:46 -0300

    fix ordinal resources

--- CSharp/Library/Microsoft.Bot.Builder/Resource/Resources.Designer.cs
+++ CSharp/Library/Microsoft.Bot.Builder/Resource/Resources.Designer.cs
@@ -340,7 +340,7 @@ namespace Microsoft.Bot.Builder.Resource {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 1=1st,first|2=2nd,second|3=3rd,third|4=4th,fourth|5=5th,fifth|6=6th,sixth|7=7th,seventh|8=8th,eigth|9=9th,ninth|10=10th,tenth.
+        ///   Looks up a localized string similar to 1=1st,first,first one|2=2nd,second,second one|3=3rd,third,third one|4=4th,fourth,fourth one|5=5th,fifth,fifth one|6=6th,sixth,sixth one|7=7th,seventh,seventh one|8=8th,eigth,eigth one|9=9th,ninth,ninth one|10=10th,tenth,tenth one.
         /// </summary>
         public static string NumberOrdinals {
             get {
