3d5b32fcaa68f0fb0e745263b5cde20869cf4651
Chris Stone <chstone@microsoft.com>
2017-05-17 14:17:51 -0400

    remove duplicate jwt check. fix for #2561

--- Node/calling/src/bots/CallConnector.ts
+++ Node/calling/src/bots/CallConnector.ts
@@ -49,6 +49,12 @@ var base64url = require('base64url');
 var keysLastFetched = 0;
 var cachedKeys: IKey[];
 var issuer: string;
+const jwtVerifyOptions = {
+    audience: this.settings.appId,
+    ignoreExpiration: false,
+    ignoreNotBefore: false,
+    clockTolerance: 300
+};
 
 export interface ICallConnectorSettings {
     callbackUrl: string;
@@ -193,35 +199,17 @@ export class CallConnector implements ucb.ICallConnector, bs.IBotStorage {
         if (token) {
             this.ensureCachedKeys((err, keys) => {
                 if (!err) {
-                    var decoded = jwt.decode(token, { complete: true });
-                    var now = new Date().getTime() / 1000;
-
-                    // verify appId, issuer, token expirs and token notBefore
-                    if (decoded.payload.aud != this.settings.appId || decoded.payload.iss != issuer || 
-                        now > decoded.payload.exp || now < decoded.payload.nbf) {
+                    try {
+                        const decoded = jwt.decode(token, { complete: true });
+                        const secret = this.getSecretForKey(decoded.header.kid);
+                        const verified = jwt.verify(token, secret, jwtVerifyOptions);
+                    } catch (err) {
+                        console.error(err.message);
                         res.status(403);
-                        res.end();   
-                    } else {
-                        var keyId = decoded.header.kid;
-                        var secret = this.getSecretForKey(keyId);
-
-                        try {
-
-                            let jwtVerifyOptions = {
-                                audience: this.settings.appId,
-                                ignoreExpiration: false,
-                                ignoreNotBefore: false,
-                                clockTolerance: 300
-                            };
-
-                            decoded = jwt.verify(token, secret, jwtVerifyOptions);
-                            this.dispatch(req.body, callback);
-                        } catch(err) {
-                            res.status(403);
-                            res.end();     
-                        }
+                        res.end();
                     }
                 } else {
+                    console.error(err.message);
                     res.status(500);
                     res.end();
                 }
