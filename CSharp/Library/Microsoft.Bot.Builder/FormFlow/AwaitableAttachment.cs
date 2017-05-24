fd503f6b553546682393b184c35bdad349f29967
maurok <maurok@gmail.com>
2017-05-24 15:29:12 -0300

    # Adding missing file headers

--- CSharp/Library/Microsoft.Bot.Builder/FormFlow/AwaitableAttachment.cs
+++ CSharp/Library/Microsoft.Bot.Builder/FormFlow/AwaitableAttachment.cs
@@ -1,4 +1,37 @@
-﻿using System;
+﻿// 
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license.
+// 
+// Microsoft Bot Framework: http://botframework.com
+// 
+// Bot Builder SDK GitHub:
+// https://github.com/Microsoft/BotBuilder
+// 
+// Copyright (c) Microsoft Corporation
+// All rights reserved.
+// 
+// MIT License:
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
 using System.IO;
 using System.Net.Http;
 using System.Runtime.Serialization;
@@ -14,6 +47,7 @@ namespace Microsoft.Bot.Builder.FormFlow.Advanced
     // TODO-MK: now we are resolving to a Stream - we could probably return stream + additional data (original name, type, etc)
     // discuss what about to return an Attachment with the Stream or byte[] in the 'object content' property
     // download attachment data for validation and store in another place?
+    // use data annotation validators as parameter to ctor and validate source?
     public class AwaitableAttachment : IAwaitable<Stream>, IAwaiter<Stream>, ISerializable
     {
         [NonSerialized]
@@ -45,6 +79,7 @@ namespace Microsoft.Bot.Builder.FormFlow.Advanced
                 ContentUrl = contentUrl,
                 Name = name
             };
+
             this.awaiter = Awaitable.FromSource(this.source, this.ResolveFromSource) as IAwaiter<Stream>;
         }
 
@@ -79,7 +114,7 @@ namespace Microsoft.Bot.Builder.FormFlow.Advanced
             throw new NotImplementedException();
         }
 
-        private async Task<Stream> ResolveFromSource(Attachment source)
+        protected virtual async Task<Stream> ResolveFromSource(Attachment source)
         {
             var stream = new MemoryStream();
 
