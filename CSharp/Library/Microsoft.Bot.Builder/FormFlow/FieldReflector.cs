6eff10bbe9f18f1c1a6c8d95644be81094332ac3
msmohan <v-muarum@microsoft.com>
2017-06-21 16:29:40 -0700

    BugFix: There was an exception thrown when generating TermsAttribute, if DescribeAttrbute description parameter is empty, space or null. This corner case caused AnnotatedSandwich sample go berserk!

--- CSharp/Library/Microsoft.Bot.Builder/FormFlow/FieldReflector.cs
+++ CSharp/Library/Microsoft.Bot.Builder/FormFlow/FieldReflector.cs
@@ -372,7 +372,7 @@ namespace Microsoft.Bot.Builder.FormFlow.Advanced
                 }
                 else
                 {
-                    _terms = new TermsAttribute(Language.GenerateTerms(_description.Description, 3));
+                    _terms = new TermsAttribute(Language.GenerateTerms((string.IsNullOrWhiteSpace(_description.Description) ? Language.CamelCase(name) : _description.Description), 3));
                 }
 
                 if (prompt != null)
