51bd8a1e8b5485561fc0cc8dee28a170d9f98fa3
Daniel Semedo <daniel.semedo@outlook.com>
2017-10-30 15:25:10 -0200

    Update PizzaOrderDialog.cs
    Comments of how to get a key in LUIS services.

--- CSharp/Samples/PizzaBot/PizzaOrderDialog.cs
+++ CSharp/Samples/PizzaBot/PizzaOrderDialog.cs
@@ -11,7 +11,8 @@ using Microsoft.Bot.Builder.Luis.Models;
 
 namespace Microsoft.Bot.Sample.PizzaBot
 {
-    [LuisModel("4311ccf1-5ed1-44fe-9f10-a6adbad05c14", "6d0966209c6e4f6b835ce34492f3e6d9", LuisApiVersion.V2)]
+//See how to get your LUIS Keys at: https://github.com/Microsoft/BotBuilder-Samples/tree/master/CSharp/intelligence-LUIS
+    [LuisModel("<Your_Model_ID", "Your_Subscription_Key", LuisApiVersion.V2)]
     [Serializable]
     class PizzaOrderDialog : LuisDialog<PizzaOrder>
     {
