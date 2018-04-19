480d91881118725edc210e97d10a7015f93b8851
Emad Elwany <emadelw@microsoft.com>
2018-04-19 14:53:15 -0700

    Emit the Luis Trace Info from the v3 C# SDK

---
+++ CSharp/Library/Microsoft.Bot.Builder/Dialogs/LuisTraceInfo.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Bot.Builder.Luis.Models;
+using Microsoft.Bot.Builder.Luis;
+using Newtonsoft.Json;
+
+namespace Microsoft.Bot.Builder.Dialogs
+{
+    public class LuisTraceInfo
+    {
+        [JsonProperty("luisResult")]
+        public LuisResult LuisResult { set; get; }
+
+        [JsonProperty("luisOptions")]
+        public ILuisOptions LuisOptions { get; set; }
+
+        [JsonProperty("luisModel")]
+        public ILuisModel LuisModel { get; set; }
+    }
+}
